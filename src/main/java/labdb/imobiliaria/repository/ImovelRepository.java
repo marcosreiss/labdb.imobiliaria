492b1332b76ae382ec9fbee52aa64c0965f67d38
marcosreiss <m.vinicius.sr11@gmail.com>
2025-02-01 21:20:54 -0300

    --add ImovelRepository ProfissionalRepository.java

--- src/main/java/labdb/imobiliaria/repository/ImovelRepository.java
+++ src/main/java/labdb/imobiliaria/repository/ImovelRepository.java
@@ -1,4 +1,32 @@
 package labdb.imobiliaria.repository;
 
+import jakarta.persistence.EntityManager;
+import labdb.imobiliaria.models.Imovel;
+import java.util.List;
+
 public class ImovelRepository {
+
+    private final EntityManager manager;
+    private DAOGenerico<Imovel> daoGenerico;
+
+    public ImovelRepository(EntityManager manager) {
+        this.manager = manager;
+        this.daoGenerico = new DAOGenerico<>(manager);
+    }
+
+    public Imovel buscarPorId(Integer id) {
+        return daoGenerico.buscaPorId(Imovel.class, id);
+    }
+
+    public List<Imovel> buscarTodos() {
+        return daoGenerico.buscarTodos(Imovel.class);
+    }
+
+    public Imovel criarOuAtualizar(Imovel imovel) {
+        return daoGenerico.salvaOuAtualiza(imovel);
+    }
+
+    public void remover(Imovel imovel) {
+        daoGenerico.remove(imovel);
+    }
 }
--- src/main/java/labdb/imobiliaria/repository/ProfissionalRepository.java
+++ src/main/java/labdb/imobiliaria/repository/ProfissionalRepository.java
@@ -1,4 +1,32 @@
 package labdb.imobiliaria.repository;
 
+import jakarta.persistence.EntityManager;
+import labdb.imobiliaria.models.Profissional;
+import java.util.List;
+
 public class ProfissionalRepository {
+
+    private final EntityManager manager;
+    private DAOGenerico<Profissional> daoGenerico;
+
+    public ProfissionalRepository(EntityManager manager) {
+        this.manager = manager;
+        this.daoGenerico = new DAOGenerico<>(manager);
+    }
+
+    public Profissional buscarPorId(Integer id) {
+        return daoGenerico.buscaPorId(Profissional.class, id);
+    }
+
+    public List<Profissional> buscarTodos() {
+        return daoGenerico.buscarTodos(Profissional.class);
+    }
+
+    public Profissional criarOuAtualizar(Profissional profissional) {
+        return daoGenerico.salvaOuAtualiza(profissional);
+    }
+
+    public void remover(Profissional profissional) {
+        daoGenerico.remove(profissional);
+    }
 }
